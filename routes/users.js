513f32e79f396dc672aa44ea6b988f71f7db6d28
Amir <amirbhai432@gmail.com>
2023-08-18 18:55:07 +0530

    work on JWT

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getAllUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
+
+// UPDATE
+router.put("/:id", updateUser);
+
+// DELETE
+router.delete("/:id", deleteUser);
+
+//Get
+router.get("/:id", getUser);
+// GET ALL
+
+router.get("/", getAllUser);
 
 export default router;
